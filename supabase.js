5e514528173ca4f07ed4f30671fda7a60eb88eef
festalucas <114783675+festalucas@users.noreply.github.com>
2026-06-26 20:23:21 -0300

    update supabase.js

--- supabase.js
+++ supabase.js
@@ -21,4 +21,53 @@ async function probarConexion(){
     console.log("ERROR SUPABASE:", error);
 }
 
-probarConexion();
+
+async function cargarReparaciones() {
+
+    const { data, error } = await clienteSupabase
+        .from("reparaciones")
+        .select("*")
+        .order("id", { ascending: true });
+
+    if (error) {
+
+        console.error(error);
+        return;
+
+    }
+
+    reparaciones = data.map(r => ({
+
+        numero: r.numero,
+        cliente: r.cliente,
+        telefono: r.telefono,
+        direccion: r.direccion,
+
+        producto: r.producto,
+        marca: r.marca,
+        modelo: r.modelo,
+        serie: r.serie,
+
+        falla: r.falla,
+        observaciones: r.observaciones,
+        accesorios: r.accesorios,
+
+        ubicacion: r.ubicacion,
+        estado: r.estado,
+
+        tecnico: r.tecnico,
+
+        valorReparacion: Number(r.valor_reparacion),
+        gastos: Number(r.gastos),
+
+        transporte: r.transporte,
+        valorTransporte: Number(r.valor_transporte),
+
+        fecha: r.fecha_creacion
+
+    }));
+
+    actualizarTablaReparaciones();
+
+}
+
