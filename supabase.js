9ba8edabafd56266bee702d5b6aa689e67fab647
festalucas <114783675+festalucas@users.noreply.github.com>
2026-06-26 11:23:22 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
 "https://jvizawmhqlwnonsdztfz.supabase.co";
 
 const SUPABASE_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2aXphd21ocWx3bm9uc2R6dGZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2NTk2OTQsImV4cCI6MjA5NjIzNTY5NH0.qAJ4W0mKmOvFMEJxNm6zG4dD2MnuToOXm2QJFKUaTQs";
+"xxxxxxxxx";
 
 const clienteSupabase =
 window.supabase.createClient(
